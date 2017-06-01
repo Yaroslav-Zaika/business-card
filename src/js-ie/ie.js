2c9b5a399eaf2daa53fbc009a9a12b93a6027cd8
Yaroslav_Zaika <yaroslav.zaika@gmail.com>
2017-06-01 22:34:12 +0300

    initial commit

--- src/js-ie/ie.js
+++
@@ -1,8 +0,0 @@
-document.createElement('ng-init');
-document.createElement('ng-click');
-document.createElement('ng-repeat');
-document.createElement('ui-view');
-document.createElement('ng:init');
-document.createElement('ng:click');
-document.createElement('ng:repeat');
-document.createElement('ui:view');
